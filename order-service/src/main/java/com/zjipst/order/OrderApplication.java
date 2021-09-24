85ae8dc66591ba37a5693b0b4516aee3c9e93763
songhao <2503521069@qq.com>
2021-09-24 17:35:53 +0800

    nacos 替换 eureka

--- order-service/src/main/java/com/zjipst/order/OrderApplication.java
+++ order-service/src/main/java/com/zjipst/order/OrderApplication.java
@@ -3,7 +3,7 @@ package com.zjipst.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,7 +11,7 @@ import org.springframework.web.client.RestTemplate;
  * @author songhao
  */
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 public class OrderApplication {
 
     public static void main(String[] args) {
